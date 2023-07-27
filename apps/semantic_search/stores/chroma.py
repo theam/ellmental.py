fa40dc6097f913a2df2499ed378f3f6f77a20ec8
BorjaZarco <borjazarco@theagilemonkeys.com>
2023-07-27 11:12:58 +0100

    add delete implementation on chroma store

--- apps/semantic_search/stores/chroma.py
+++ apps/semantic_search/stores/chroma.py
@@ -1,37 +1,47 @@
-from typing import Optional, Any
+import json
+import uuid
+from typing import Any, Optional
 
-from pydantic import BaseModel
-from common.utils import flatten
-from stores.base import SearchResult
 from chromadb import Client, PersistentClient
 from chromadb.config import Settings
 from chromadb.types import Metadata, Where
-import uuid
-import json
+from common.utils import flatten
+from pydantic import BaseModel
+from stores.base import EmbeddingsStore, SearchResult, StoreRequest
 
-from stores.base import EmbeddingsStore, StoreRequest
 
 class ChromaEmbeddingsStoreSettings(BaseModel):
     host: str
     port: str
     collection: str
 
+
 class ChromaEmbeddingsStore(EmbeddingsStore):
-    def __init__(self, path: Optional[str] = None, settings: Optional[ChromaEmbeddingsStoreSettings] = None) -> None:
+    def __init__(
+        self,
+        path: Optional[str] = None,
+        settings: Optional[ChromaEmbeddingsStoreSettings] = None,
+    ) -> None:
         if path is not None:
-            self.client = PersistentClient(path=path, settings=Settings(anonymized_telemetry=False))
+            self.client = PersistentClient(
+                path=path, settings=Settings(anonymized_telemetry=False)
+            )
         elif settings is not None:
-            self.client = Client(Settings(
-                chroma_api_impl="rest",
-                chroma_server_host=settings.host,
-                chroma_server_http_port=settings.port,
-                anonymized_telemetry=False
-            ))
+            self.client = Client(
+                Settings(
+                    chroma_api_impl="rest",
+                    chroma_server_host=settings.host,
+                    chroma_server_http_port=settings.port,
+                    anonymized_telemetry=False,
+                )
+            )
         else:
             raise Exception("Missing path or settings")
-        
+
         collection_name = "embeddings" if settings is None else settings.collection
-        self.collection = self.client.get_or_create_collection(collection_name, metadata={ "hnsw:space": "ip" })
+        self.collection = self.client.get_or_create_collection(
+            collection_name, metadata={"hnsw:space": "ip"}
+        )
 
     def store(self, embeddings: list[StoreRequest]) -> list[str]:
         ids = []
@@ -48,39 +58,80 @@ class ChromaEmbeddingsStore(EmbeddingsStore):
             embeddings_items.append(embedding.embedding)
             metadatas.append(metadata)
 
-        self.collection.upsert(ids=ids, embeddings=embeddings_items, metadatas=metadatas)
+        self.collection.upsert(
+            ids=ids, embeddings=embeddings_items, metadatas=metadatas
+        )
         return ids
 
-    def search(self, embedding: list[float], cluster_ids: list[str], match_threshold: float = 0.8, limit: int = 10) -> list[SearchResult]:
+    def search(
+        self,
+        embedding: list[float],
+        cluster_ids: list[str],
+        match_threshold: float = 0.8,
+        limit: int = 10,
+    ) -> list[SearchResult]:
+        filters: Optional[Where] = self._generate_in_clause("cluster_id", cluster_ids)
 
-        filters: Optional[Where] = None if not cluster_ids else { 
-            "cluster_id": { "$eq": cluster_ids[0] } 
-        } if len(cluster_ids) == 1 else { "$or": [
-            { "cluster_id": { "$eq": cluster_id } 
-        } for cluster_id in cluster_ids ]}
-
-        result = self.collection.query(query_embeddings=[embedding], n_results=limit, where=filters, include=["metadatas", "distances"])
+        result = self.collection.query(
+            query_embeddings=[embedding],
+            n_results=limit,
+            where=filters,
+            include=["metadatas", "distances"],
+        )
 
         matches: list[SearchResult] = []
 
-        if result["ids"] is None or result["metadatas"] is None or result["distances"] is None:
+        if (
+            result["ids"] is None
+            or result["metadatas"] is None
+            or result["distances"] is None
+        ):
             raise Exception("Error searching: No rows found")
 
-        for id, metadata, distance in zip(flatten(result["ids"]), flatten(result["metadatas"]), flatten(result["distances"])):
-            cluster_id: str | None = str(metadata.get('cluster_id', None))
-            raw_metadata = metadata.get('metadata', '{}')
+        for id, metadata, distance in zip(
+            flatten(result["ids"]),
+            flatten(result["metadatas"]),
+            flatten(result["distances"]),
+        ):
+            cluster_id: str | None = str(metadata.get("cluster_id", None))
+            raw_metadata = metadata.get("metadata", "{}")
             real_metadata: dict[str, Any] = {}
             if isinstance(raw_metadata, str):
                 real_metadata = json.loads(raw_metadata)
 
-            matches.append(SearchResult(
-                id=id,
-                metadata=real_metadata,
-                score=self.__cosine_distance_to_normalized_similarity(distance),
-                cluster_id=cluster_id
-            ))
-            
+            matches.append(
+                SearchResult(
+                    id=id,
+                    metadata=real_metadata,
+                    score=self.__cosine_distance_to_normalized_similarity(distance),
+                    cluster_id=cluster_id,
+                )
+            )
+
         return matches
+
+    def delete(self, cluster_ids: list[str]) -> bool:
+        where_clause: Optional[Where] = self._generate_in_clause(
+            "cluster_id", cluster_ids
+        )
+
+        self.collection.delete(where=where_clause)
+        return True
+
+    def _generate_in_clause(
+        self, filter_key: str, filter_values: list[str]
+    ) -> Optional[Where]:
+        if not filter_values:
+            return None
+
+        if len(filter_values) == 1:
+            return {filter_key: {"$eq": filter_values[0]}}
+
+        return {
+            "$or": [
+                {filter_key: {"$eq": filter_value}} for filter_value in filter_values
+            ]
+        }
 
     def __cosine_distance_to_normalized_similarity(self, distance: float) -> float:
         similarity = 1 - distance
